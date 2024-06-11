43c307a09c8ce6180936528cdc304d709d1ddb8e
zakwan0497 <zakwan0497@gmail.com>
2024-06-12 00:38:48 +0800

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CheckIn from './Components/CheckIn'
+
+function App() {
+
+  return (
+    <><CheckIn/></>
+     
+  )
+}
+
+export default App
---
+++ src/Components/CheckIn.jsx
@@ -0,0 +1,28 @@
+import './style.css'
+
+const CheckIn = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 checkInPage'>
+      <div className='p-3 rounded w-25 border checkInForm'>
+        <h2>Popcorn</h2>
+        <h2>Check In Page</h2>
+        <form>
+          <div>
+            <label htmlFor="employeeId"></label>
+            <input type="text" name="employeeId" autoComplete="off" placeholder="Enter Employee ID" className='form-control rounded-0' />
+          </div>
+          <div>
+            <label htmlFor="password"></label>
+            <input type="password" name="password" placeholder="Password" className='form-control rounded-0' />
+          </div>
+          <button className='btn btn-success w-100 rounded-0'>Check In</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default CheckIn
+
+
+// nanti replace all bootstrap components with tailwindcss
